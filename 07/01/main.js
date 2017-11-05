bb503b578d68dc71c672b8c7924b1890c19cccc8
OneStepCloser <artinspira@gmail.com>
2017-11-06 00:08:11 +0300

    i've started

---
+++ 07/01/main.js
@@ -0,0 +1,12 @@
+alert('hi');
+/*
+function telChangeHandler() {
+  alert('hi');
+}
+
+const telInput = document.getElementById('wrapper__phone');
+
+alert('hi');
+telInput.addEventListener('oninput', telChangeHandler);
+*/
+
