6d4f86f9591b2389d6aa0e509ca4e0071644eb3a
OneStepCloser <artinspira@gmail.com>
2017-11-06 21:13:07 +0300

    1 task done

--- 07/01/main.js
+++ 07/01/main.js
@@ -1,12 +1,58 @@
-alert('hi');
-/*
+function getDigitSequence(charSequence) {
+  const digitRegexp = new RegExp('[0-9]');
+  const subLines = charSequence.split('');
+  let result = '';
+
+  for (let i = 0; i < subLines.length; ++i) {
+    if (digitRegexp.test(subLines[i])) {
+      result += subLines[i];
+    }
+  }
+
+  return result;
+}
+
+function getMaskedNumber(digitSequence) {
+  const subLines = digitSequence.split('');
+
+  let result = '';
+
+  for (let i = 0; i < subLines.length; ++i) {
+    if (i === 0) { result += '+'; }
+    if (i === 1) { result += '('; }
+    if (i === 4) { result += ')-'; }
+    if (i === 7) { result += '-'; }
+    if (i === 9) { result += '-'; }
+    result += subLines[i];
+  }
+
+  return result;
+}
+
 function telChangeHandler() {
-  alert('hi');
+  const val = this.value;
+
+  if (val.length < 2) {
+    this.value = '+7';
+    return;
+  }
+
+  if (val.length === 17) {
+    const link = document.getElementById('link');
+
+    link.text = `Позвонить на ${val}`;
+    link.href = `tel:${val}`;
+    return;
+  }
+
+  if (val.length > 17) {
+    this.value = val.substr(0, val.length - 1);
+    return;
+  }
+
+  this.value = getMaskedNumber(getDigitSequence(val));
 }
 
 const telInput = document.getElementById('wrapper__phone');
 
-alert('hi');
-telInput.addEventListener('oninput', telChangeHandler);
-*/
-
+telInput.addEventListener('input', telChangeHandler);
